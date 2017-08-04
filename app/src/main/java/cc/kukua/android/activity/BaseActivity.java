5597fbf47ab1ab2a343c4c1a2dc2b273db165728
Acellam Guy <abiccel@yahoo.com>
2017-08-04 17:00:30 +0300

    Add menu inflater

--- app/src/main/java/cc/kukua/android/activity/BaseActivity.java
+++ app/src/main/java/cc/kukua/android/activity/BaseActivity.java
@@ -2,9 +2,13 @@ package cc.kukua.android.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import net.hockeyapp.android.CrashManager;
 import net.hockeyapp.android.UpdateManager;
+
+import cc.kukua.android.R;
 
 /**
  * Created by mistaguy on 8/4/2017.
@@ -54,6 +58,12 @@ public class BaseActivity  extends AppCompatActivity {
     public void onDestroy() {
         super.onDestroy();
         unregisterManagers();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
 }
--- app/src/main/java/cc/kukua/android/activity/FirstTimeActivity.java
+++ app/src/main/java/cc/kukua/android/activity/FirstTimeActivity.java
@@ -8,7 +8,7 @@ import cc.kukua.android.R;
 public class FirstTimeActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_time);
     }
