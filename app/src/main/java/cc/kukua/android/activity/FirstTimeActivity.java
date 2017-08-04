ecb29d531288d1b6d5140c09b050e57524945908
Acellam Guy <abiccel@yahoo.com>
2017-08-04 12:37:17 +0300

    apply theme

--- app/src/main/java/cc/kukua/android/activity/FirstTimeActivity.java
+++ app/src/main/java/cc/kukua/android/activity/FirstTimeActivity.java
@@ -1,15 +1,33 @@
 package cc.kukua.android.activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import cc.kukua.android.R;
 
 public class FirstTimeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_time);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
