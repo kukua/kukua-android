8aa6ef40fcac3bb5362df4a601299ecd91fb1fd1
Acellam Guy <abiccel@yahoo.com>
2017-08-04 14:59:20 +0300

    Check for updates

--- app/src/main/java/cc/kukua/android/activity/BaseActivity.java
+++ app/src/main/java/cc/kukua/android/activity/BaseActivity.java
@@ -1,14 +1,27 @@
 package cc.kukua.android.activity;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.UpdateManager;
 
 /**
  * Created by mistaguy on 8/4/2017.
  */
 
 public class BaseActivity  extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Your own code to create the view
+        // ...
+
+        // TODO check for updates every week
+        checkForUpdates();
+    }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -18,6 +31,29 @@ public class BaseActivity  extends AppCompatActivity {
 
     private void checkForCrashes() {
         CrashManager.register(this);
+    }
+
+
+    private void checkForUpdates() {
+        // Remove this for store builds!
+        UpdateManager.register(this);
+    }
+
+    private void unregisterManagers() {
+        UpdateManager.unregister();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterManagers();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterManagers();
     }
 
 }
