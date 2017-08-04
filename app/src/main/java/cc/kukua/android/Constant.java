3261d2ce94c114d985a6fac26c05c20c51171f55
Acellam Guy <abiccel@yahoo.com>
2017-08-04 14:21:29 +0300

    add application insights

---
+++ app/src/main/java/cc/kukua/android/Constant.java
@@ -0,0 +1,10 @@
+package cc.kukua.android;
+
+/**
+ * Created by mistaguy on 8/4/2017.
+ */
+
+public class Constant {
+    static String  azureUrl ="https://kukua-android.azurewebsites.net";
+}
+
--- app/src/main/java/cc/kukua/android/KukuaApp.java
+++ app/src/main/java/cc/kukua/android/KukuaApp.java
@@ -1,17 +1,25 @@
 package cc.kukua.android;
 
 import android.app.Application;
+import com.microsoft.windowsazure.mobileservices.*;
 
 /**
  * Created by mistaguy on 7/28/2017.
  */
 
 public class KukuaApp extends Application {
-
+    private MobileServiceClient mClient;
     @Override
     public void onCreate() {
         super.onCreate();
 
         // Initialize logging first to log all operations
+        try {
+            mClient = new MobileServiceClient(
+                    Constant.azureUrl,
+                    this
+            );
+        }catch (Exception ex){
+        }
     }
 }
