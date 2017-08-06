c32448a8160187026d7bb64effeabfd3e3c51dd0
Acellam Guy <abiccel@yahoo.com>
2017-08-06 16:08:51 +0300

    Anengine initial

---
+++ app/src/main/java/cc/kukua/android/activity/weather/DayActivity.java
@@ -0,0 +1,15 @@
+package cc.kukua.android.activity.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cc.kukua.android.R;
+
+public class DayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day);
+    }
+}
