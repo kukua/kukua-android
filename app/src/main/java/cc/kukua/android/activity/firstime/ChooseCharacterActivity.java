8076bb0c4221c0139b7232b1675a828193991348
Acellam Guy <abiccel@yahoo.com>
2017-08-21 15:17:45 +0300

    Add second preview

--- app/src/main/java/cc/kukua/android/activity/firstime/ChooseCharacterActivity.java
+++ app/src/main/java/cc/kukua/android/activity/firstime/ChooseCharacterActivity.java
@@ -5,7 +5,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,6 +19,10 @@ public class ChooseCharacterActivity extends FragmentActivity {
 
     @BindView(R.id.viewpager)
     ViewPager pager;
+    @BindView(R.id.left_nav)
+    ImageView leftNav;
+    @BindView(R.id.right_nav)
+    ImageView rightNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,29 @@ public class ChooseCharacterActivity extends FragmentActivity {
         ButterKnife.bind(this);
 
         pager.setAdapter(new ChooseCharacterPagerAdapter(getSupportFragmentManager()));
+
+        leftNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int tab = pager.getCurrentItem();
+                if (tab > 0) {
+                    tab--;
+                    pager.setCurrentItem(tab);
+                } else if (tab == 0) {
+                    pager.setCurrentItem(tab);
+                }
+            }
+        });
+
+        // Images right navigatin
+        rightNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int tab = pager.getCurrentItem();
+                tab++;
+                pager.setCurrentItem(tab);
+            }
+        });
     }
 
 }
