c87f9f9e0b7d4de26a325e9893bb1157b4ecf541
Acellam Guy <abiccel@yahoo.com>
2017-08-21 16:06:49 +0300

    finish choose character mockup

--- app/src/main/java/cc/kukua/android/activity/firstime/ChooseCharacterActivity.java
+++ app/src/main/java/cc/kukua/android/activity/firstime/ChooseCharacterActivity.java
@@ -5,9 +5,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,7 +19,7 @@ import cc.kukua.android.R;
 import cc.kukua.android.adapters.ChooseCharacterPagerAdapter;
 
 
-public class ChooseCharacterActivity extends FragmentActivity {
+public class ChooseCharacterActivity extends AppCompatActivity {
 
     @BindView(R.id.viewpager)
     ViewPager pager;
@@ -23,12 +27,16 @@ public class ChooseCharacterActivity extends FragmentActivity {
     ImageView leftNav;
     @BindView(R.id.right_nav)
     ImageView rightNav;
+    @BindView(R.id.toolbar_title)
+    TextView tvToolbarTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_character);
         ButterKnife.bind(this);
+        
+        tvToolbarTitle.setText("choose your character");
 
         pager.setAdapter(new ChooseCharacterPagerAdapter(getSupportFragmentManager()));
 
