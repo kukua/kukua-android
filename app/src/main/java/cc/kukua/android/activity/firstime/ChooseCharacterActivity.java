261cf62c6992320cd1ed4d5c341a79674c5c1ff9
calistus <icalistus@gmail.com>
2017-08-28 11:50:47 +0100

    Get Selected character_id Get purpose

--- app/src/main/java/cc/kukua/android/activity/firstime/ChooseCharacterActivity.java
+++ app/src/main/java/cc/kukua/android/activity/firstime/ChooseCharacterActivity.java
@@ -17,6 +17,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import cc.kukua.android.R;
 import cc.kukua.android.adapters.ChooseCharacterPagerAdapter;
+import cc.kukua.android.constants.DummyDataProvider;
+import cc.kukua.android.utils.LogUtils;
+import cc.kukua.android.utils.UiUtils;
 
 
 public class ChooseCharacterActivity extends AppCompatActivity {
@@ -29,7 +32,10 @@ public class ChooseCharacterActivity extends AppCompatActivity {
     ImageView rightNav;
     @BindView(R.id.toolbar_title)
     TextView tvToolbarTitle;
+    @BindView(R.id.btn_next3)
+    Button btnNextButton;
 
+    private String TAG = ChooseCharacterActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,9 +52,11 @@ public class ChooseCharacterActivity extends AppCompatActivity {
                 int tab = pager.getCurrentItem();
                 if (tab > 0) {
                     tab--;
+                    DummyDataProvider.userDetail.put("character",String.valueOf(tab));
                     pager.setCurrentItem(tab);
                 } else if (tab == 0) {
                     pager.setCurrentItem(tab);
+                    DummyDataProvider.userDetail.put("character",String.valueOf(tab));
                 }
             }
         });
@@ -60,6 +68,12 @@ public class ChooseCharacterActivity extends AppCompatActivity {
                 int tab = pager.getCurrentItem();
                 tab++;
                 pager.setCurrentItem(tab);
+            }
+        });
+        btnNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogUtils.log(TAG,DummyDataProvider.userDetail.toString());
             }
         });
     }
