630e7152f5067e436a2b4ffd50a4e17058aa14e5
calistus <icalistus@gmail.com>
2017-08-31 02:26:35 +0100

    Update APIService class remove commented codes update requestweatherforecast function

--- app/src/main/java/cc/kukua/android/retrofit/APIService.java
+++ app/src/main/java/cc/kukua/android/retrofit/APIService.java
@@ -12,6 +12,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
@@ -21,17 +22,7 @@ import retrofit2.http.Query;
  */
 
 public interface APIService {
-    /* @FormUrlEncoded
-     @POST("v1.0/register")
-     Call<RegisterResponseModel> userSignUp(@Field("first_name") String full_name,
-                                            @Field("last_name") String email,
-                                            @Field("email") String phone,
-                                            @Field("phone_number") String phoneNumber,
-                                            @Field("password") String password,
-                                            @Field("character_id") String characterID,
-                                            @Field("location") String location,
-                                            @Field("purpose_id") String purposeID);
- */
+
     @Headers("Content-Type: application/json")
     @POST("api/user/login")
     Call<LoginResponseModel> login(@Body String body);
@@ -40,19 +31,13 @@ public interface APIService {
     @POST("/api/user/create")
     Call<RegisterResponseModel> register(@Body RegisterQueryModel body);
 
-    /* @FormUrlEncoded
-     @POST("v1.0/login")
-     Call<LoginResponseModel> login(@Field("email") String email, @Field("password") String password);
- */
     @FormUrlEncoded
     @POST("v1.0/sendsms")
     Call<SendSmsResponseModel> sendsms(@Field("phonenumber") String phonenumber, @Field("message") String message);
 
-    @FormUrlEncoded
-    @POST("v1.0/requestweatherforecast")
-    Call<RequestForecastResponseModel> requestweatherforecast(@Field("latitude") float latitude,
-                                                              @Field("longtitude") float longtitude,
-                                                              @Query("locality") String order);
+    @Headers("Content-Type: application/json")
+    @POST("api/forecast/create")
+    Call<RequestForecastResponseModel> requestweatherforecast(@Body String body);
 
     @FormUrlEncoded
     @POST("v1.0/dailyweatherforecast")
