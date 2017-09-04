77bf431f2bc8b8cd26ff03f8d2090b3d6195f822
calistus <icalistus@gmail.com>
2017-09-04 23:17:37 +0100

    Update Login Response Model

--- app/src/main/java/cc/kukua/android/model/server_response_model/LoginResponseModel.java
+++ app/src/main/java/cc/kukua/android/model/server_response_model/LoginResponseModel.java
@@ -4,6 +4,7 @@ package cc.kukua.android.model.server_response_model;
  * Created by calistus on 30/07/2017.
  */
 
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -15,6 +16,18 @@ public class LoginResponseModel {
     @SerializedName("login")
     @Expose
     private Boolean login;
+    @SerializedName("user_id")
+    @Expose
+    private String userId;
+    @SerializedName("lat")
+    @Expose
+    private Float lat;
+    @SerializedName("lon")
+    @Expose
+    private Float lon;
+    @SerializedName("timezone")
+    @Expose
+    private String timezone;
 
     public Integer getState() {
         return state;
@@ -32,12 +45,47 @@ public class LoginResponseModel {
         this.login = login;
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Float getLat() {
+        return lat;
+    }
+
+    public void setLat(Float lat) {
+        this.lat = lat;
+    }
+
+    public Float getLon() {
+        return lon;
+    }
+
+    public void setLon(Float lon) {
+        this.lon = lon;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
     @Override
     public String toString() {
         return "LoginResponseModel{" +
                 "state=" + state +
-                ", login='" + login + '\'' +
+                ", login=" + login +
+                ", userId='" + userId + '\'' +
+                ", lat=" + lat +
+                ", lon=" + lon +
+                ", timezone='" + timezone + '\'' +
                 '}';
     }
-
 }
