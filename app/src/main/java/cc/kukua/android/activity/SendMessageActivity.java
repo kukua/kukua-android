0594c19d8758cf7f62f1fd46a604fe7a3adf997c
calistus <icalistus@gmail.com>
2017-09-07 22:07:27 +0100

    Touch SMS Body UI

--- app/src/main/java/cc/kukua/android/activity/SendMessageActivity.java
+++ app/src/main/java/cc/kukua/android/activity/SendMessageActivity.java
@@ -1,10 +1,17 @@
 package cc.kukua.android.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,6 +28,10 @@ public class SendMessageActivity extends AppCompatActivity {
     @BindView(R.id.btn_send)
     Button btnSend;
 
+    String phoneNo;
+    String message;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +39,54 @@ public class SendMessageActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         toolbarTitle.setText(R.string.send_sms);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMSMessage();
+            }
+        });
+    }
+
+    protected void sendSMSMessage() {
+        phoneNo = etPhone.getText().toString();
+        message = etMessage.getText().toString();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                        Toast.makeText(getApplicationContext(), "SMS sent.",
+                                Toast.LENGTH_LONG).show();
+
+                    }catch (Exception e){
+                        Toast.makeText(this, getString(R.string.network_error), Toast.LENGTH_SHORT).show();
+                    }
+                    } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+
     }
 }
